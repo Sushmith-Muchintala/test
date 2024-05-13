07eb61caa1169b32e5c80f1be2046b1061c27773
Sushmith <sushmith@site-iq.com>
2024-05-13 17:10:50 -0500

    pushing initial go files.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+
+}
